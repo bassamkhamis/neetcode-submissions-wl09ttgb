1c0b502d0611015fcc9811a435a9c392623ad120
bassam khamis <61948325+bassamkhamis@users.noreply.github.com>
2026-04-04 16:31:35 +0200

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.cpp
@@ -0,0 +1,48 @@
+class TrieNode{
+public:
+    unordered_map<char, TrieNode*> children;
+    bool endOfWord;
+};
+class WordDictionary {
+    TrieNode* root;
+    bool dfs(TrieNode* cur, string word, int j) {
+       for (int i = j; i < word.size(); i++) {
+            char c = word[i];
+            if (c == '.') {
+                for (auto [_, child] : cur->children) {
+                    if (child && dfs(child, word, i + 1))
+                        return true;
+                }
+                return false;
+            }
+            else {
+                if (cur->children.find(c) == cur->children.end()) 
+                    return false;
+                
+                cur = cur->children[c]; 
+            }   
+       }
+
+       return cur->endOfWord;    
+    }
+public:
+    WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    void addWord(string word) {
+       auto cur = root;
+       for (char c : word) {
+            if (cur->children.find(c) == cur->children.end()) 
+                cur->children[c] = new TrieNode();
+
+            cur = cur->children[c];    
+       } 
+
+       cur->endOfWord = true;
+    }
+    
+    bool search(string word) {
+        return dfs(root, word, 0);
+    }
+};
