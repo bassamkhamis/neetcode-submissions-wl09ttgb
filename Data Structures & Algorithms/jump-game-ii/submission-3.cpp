fc66cfec393f4188b350dc7d52c9f5ff8ccfb59e
bassam khamis <61948325+bassamkhamis@users.noreply.github.com>
2026-04-11 19:52:51 +0200

    Add: jump-game-ii - submission-3

---
+++ Data Structures & Algorithms/jump-game-ii/submission-3.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    vector<int> mem;
+    int dp(vector<int>& nums, int idx) {
+        if (idx == (nums.size() - 1))
+            return 0;
+
+        int& res = mem[idx];
+        if (res != -1)  
+            return res;
+
+        int end = std::min((int)(nums.size() - 1), nums[idx]);
+        res = nums.size();
+
+        for (int i = 1; i <= end; i++) {
+            int cur = dp(nums, idx + i);
+            if (cur != -1)
+                res = std::min(res, 1 + cur);
+        }
+
+        if (res == nums.size())
+            res = -1;
+
+        return res;
+    }
+public:
+    int jump(vector<int>& nums) {
+        mem = vector<int>(nums.size(), -1);
+        return dp(nums, 0);
+    }
+};
