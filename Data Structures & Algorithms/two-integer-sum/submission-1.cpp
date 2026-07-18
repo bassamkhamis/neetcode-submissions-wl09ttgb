44578d2d5e4652e444be5c4c11412ea0d5fedb33
bassam khamis <61948325+bassamkhamis@users.noreply.github.com>
2026-07-18 06:29:13 +0300

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int, int>> A;
+        for (int i = 0; i < nums.size(); i++) {
+            A.push_back({nums[i], i});
+        }
+
+        // set projection argument to the first in pair 
+        std::ranges::sort(A, std::less{}, &std::pair<int, int>::first);
+
+        int i = 0, j = nums.size() - 1;
+        while (i < j) {
+            int cur = A[i].first + A[j].first;
+            if (cur == target) {
+                return {min(A[i].second, A[j].second),
+                        max(A[i].second, A[j].second)};
+            } else if (cur < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return {};
+    }
+};
